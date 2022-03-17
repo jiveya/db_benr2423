1bf6f50b0c876cff16c3ebcbbccace37a9cc9c30
jiveya <101575790+jiveya@users.noreply.github.com>
2022-03-17 11:53:28 +0800

    Create code1_week02.js

---
+++ code1_week02.js
@@ -0,0 +1,11 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.lhdjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    if(err) {
+        console.log(err.message)
+        return
+    }
+     console.log('Connected to MongoDB');
+    })
